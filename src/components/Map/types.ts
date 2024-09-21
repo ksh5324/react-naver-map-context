12910151a505ef3c7744e0f035fa49e947e76ae0
seohyeon1578 <seohyeon1578@naver.com>
2024-09-21 16:50:20 +0900

    docs: fix jsdoc with @description for better dx

--- src/components/Map/types.ts
+++ src/components/Map/types.ts
@@ -2,16 +2,26 @@ import { CSSProperties, HTMLAttributes, PropsWithChildren } from "react";
 import { OptionalRecord } from "../../@types/generic";
 import { NaverEventFunctionObject } from "../../@types/NaverEvent";
 
-/** Marker를 정의하는 옵션. */
+/**
+ * @description Marker를 정의하는 옵션.
+ */
 export type MapProps = PropsWithChildren<
   {
-    /** 지도를 삽입할 HTML 요소의 id입니다. */
+    /**
+     * @description 지도를 삽입할 HTML 요소의 id입니다.
+     */
     mapId: string;
-    /** 지도의 옵션 객체입니다. */
+    /**
+     * @description 지도의 옵션 객체입니다.
+     */
     mapOptions?: naver.maps.MapOptions;
-    /** 지도 요소의 style입니다. */
+    /**
+     * @description 지도 요소의 style입니다.
+     */
     style?: CSSProperties;
-    /** 지도 요소의 className입니다. */
+    /**
+     * @description 지도 요소의 className입니다.
+     */
     className?: HTMLAttributes<HTMLDivElement>["className"];
   } & OptionalRecord<keyof NaverEventFunctionObject, (e: any) => any>
 >;
