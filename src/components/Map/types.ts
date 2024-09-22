a9649fa79c93e4c718952e865da304f699060ca5
김서현 <seohyeon1578@naver.com>
2024-09-22 14:48:14 +0900

    #20 docs: map props jsdoc 추가 (#21)
    * docs: map props jsdoc 추가
    
    * docs: fix jsdoc with @description for better dx

--- src/components/Map/types.ts
+++ src/components/Map/types.ts
@@ -2,11 +2,26 @@ import { CSSProperties, HTMLAttributes, PropsWithChildren } from "react";
 import { OptionalRecord } from "../../@types/generic";
 import { NaverEventFunctionObject } from "../../@types/NaverEvent";
 
+/**
+ * @description Marker를 정의하는 옵션.
+ */
 export type MapProps = PropsWithChildren<
   {
+    /**
+     * @description 지도를 삽입할 HTML 요소의 id입니다.
+     */
     mapId: string;
+    /**
+     * @description 지도의 옵션 객체입니다.
+     */
     mapOptions?: naver.maps.MapOptions;
+    /**
+     * @description 지도 요소의 style입니다.
+     */
     style?: CSSProperties;
+    /**
+     * @description 지도 요소의 className입니다.
+     */
     className?: HTMLAttributes<HTMLDivElement>["className"];
   } & OptionalRecord<keyof NaverEventFunctionObject, (e: any) => any>
 >;
