f9e916781e57bcb8072c3378bdfe1ef446289c7c
seohyeon1578 <seohyeon1578@naver.com>
2025-01-03 19:10:32 +0900

    feat: rollup config 세팅

---
+++ rollup.config.js
@@ -0,0 +1,82 @@
+import esbuild from 'rollup-plugin-esbuild';
+import resolve from '@rollup/plugin-node-resolve';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import {createRequire} from 'module';
+
+const packageJson = createRequire(import.meta.url)('./package.json');
+const extensions = ['.ts', '.tsx'];
+const external = [...Object.keys(packageJson.dependencies || {}), ...Object.keys(packageJson.peerDependencies || {})];
+
+function getEsbuild() {
+    return esbuild({
+        target: 'es2019',
+        jsx: 'automatic',
+        minify: true,
+        treeShaking: true,
+        tsconfig: 'tsconfig.json',
+    });
+}
+
+function createDeclarationConfig(input, output) {
+  return {
+    input,
+    output: {
+      dir: output,
+    },
+    external,
+    plugins: [
+      typescript({
+        declaration: true,
+        emitDeclarationOnly: true,
+        outDir: output,
+      }),
+      commonjs(),
+      resolve(),
+    ],
+  };
+}
+
+function createESMConfig(input, output) {
+  return {
+    input,
+    output: {
+      file: output,
+      format: 'esm',
+    },
+    external,
+    plugins: [
+      peerDepsExternal(),
+      resolve({ extensions }),
+      terser(),
+      getEsbuild(),
+    ],
+  }
+}
+
+function createCJSConfig(input, output) {
+  return {
+    input,
+    output: {
+      file: output,
+      format: 'cjs',
+    },
+    external,
+    plugins: [
+      peerDepsExternal(),
+      resolve({ extensions }),
+      terser(),
+      getEsbuild(),
+    ],
+  }
+}
+
+const config = [
+  createCJSConfig('src/index.ts', 'dist/index.js'),
+  createESMConfig('src/index.ts', 'dist/index.esm.js'),
+  createDeclarationConfig('src/index.ts', 'dist'),
+];
+
+export default config;
